6ad4e8c4b46f8653410528dbadd39bc363c4a90d
sakshamsd <saksham@pegotec.net>
2022-09-24 16:22:49 +0545

    admin setup completed

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,121 @@
-from django.contrib import admin
+from pyexpat.errors import messages
+from django.contrib import admin, messages
 
+from . import models
+from django.http.request import HttpRequest
+from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+from django.db.models.query import QuerySet
 # Register your models here.
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    # exclude = ['promotions']
+    # readonly_fields = ['title']
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    actions = ['clear_inventory']
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_per_page = 10
+    list_select_related = ['collection']
+    list_filter = ["collection", 'last_update', InventoryFilter]
+    search_fields = ['title']
+    # inlines = [TagInline]
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
+    @admin.action(description='Clear Inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request, f'{updated_count} products were successfully updated', messages.ERROR)
+
+# admin.site.register(Product, ProductAdmin) instead if this we use decorater like above for easu method
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership', 'order_count']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+    # __startswith is lookup and i is ued for cas insensetive if i removed it will be case sensitive
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+    @admin.display(ordering='order_count')
+    def order_count(self, customer):
+        # admin: app_model_page
+        url = (reverse('admin:store_order_changelist') + "?" +
+               urlencode({'customer__id': str(customer.id)}))
+        return format_html('<a href="{}">{}</a>', url, customer.order_count)
+
+    def get_queryset(self, request: HttpRequest):  # returns queryset
+        return super().get_queryset(request).annotate(
+            order_count=Count('order')
+        )
+
+
+class OrderItemInline(admin.TabularInline):  # admin.StackInline
+    # autocomplete_fields = ['product']
+    autocomplete_fields = ['product']
+    min_num = 1
+    max_num = 10
+    model = models.OrderItem
+    extra = 0
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['placed_at', 'payment_status', 'customer']
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
+
+    # def customer_name(self, order):
+    #     name = order.customer.first_name + " " + order.customer.last_name
+    #     return name
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+    search_fields = ['title']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        # admin: app_model_page
+        url = (reverse('admin:store_product_changelist') + "?" +
+               urlencode({'collection__id': str(collection.id)}))
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+        # return collection.products_count
+
+    def get_queryset(self, request: HttpRequest):  # returns queryset
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
--- store/models.py
+++ store/models.py
@@ -1,6 +1,5 @@
-from xml.parsers.expat import model
 from django.db import models
-
+from django.core.validators import MinValueValidator
 # Create your models here.
 
 
@@ -14,16 +13,31 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')  # + implies do not care about circular relationship
 
+    def __str__(self) -> str:
+        # return super().__str__()
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(null=True, blank=True)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2, validators=[
+                                     MinValueValidator(1, message="Must be greater 1")])
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion)
+    promotions = models.ManyToManyField(Promotion, blank=True)
+
+    def __str__(self) -> str:
+        # return super().__str__()
+        return self.title
+
+    class Meta:
+        ordering = ['title']
 
 
 class Customer(models.Model):
@@ -45,6 +59,12 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
 
 
 class Address(models.Model):
